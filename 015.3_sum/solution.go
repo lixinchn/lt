6f47d9db436a09ae559e91837a2c0fced86d1d13
lixin <lixin@windimg.com>
2020-09-09 18:21:53 +0800

    改一下

--- 015.3_sum/solution.go
+++ 015.3_sum/solution.go
@@ -5,35 +5,27 @@ import (
 )
 
 func threeSum(nums []int) [][]int {
-	m := make([][]int, 0)
-	t := make(map[int]bool)
+	m := [][]int{}
+	t := map[int]bool{}
 	sort.Ints(nums)
 	for i := 0; i < len(nums); i++ {
 		if _, ok := t[nums[i]]; !ok {
-			if r := twoSum(nums, nums[i]*-1, i, t); len(r) != 0 {
-				m = append(m, r...)
+			mm := map[int]int{}
+			prevAnother := 10000000
+			for j := i + 1; j < len(nums); j++ {
+				another := (nums[i] + nums[j]) * -1
+				if prevAnother != another {
+					if _, ok := mm[another]; ok {
+						prevAnother = another
+						m = append(m, []int{nums[j], nums[i], another})
+						t[nums[i]] = true
+					}
+				}
+				mm[nums[j]] = i
 			}
 		}
 	}
 	return m
-}
-
-func twoSum(nums []int, target int, index int, t map[int]bool) [][]int {
-	m := make(map[int]int)
-	r := make([][]int, 0)
-	prevAnother := 10000000
-	for i := index + 1; i < len(nums); i++ {
-		another := target - nums[i]
-		if prevAnother != another {
-			if _, ok := m[another]; ok {
-				prevAnother = another
-				r = append(r, []int{nums[i], target * -1, another})
-				t[target*-1] = true
-			}
-		}
-		m[nums[i]] = i
-	}
-	return r
 }
 
 // -4, -1, -1, 0, 1, 2
