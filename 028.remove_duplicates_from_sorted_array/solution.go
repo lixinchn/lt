411e8196012f3928a7c8e11f810aa46d92f32c1b
lixin <lixin@windimg.com>
2020-09-10 18:42:03 +0800

    remove duplicate from sorted array

---
+++ 028.remove_duplicates_from_sorted_array/solution.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[j] == nums[i] {
+			continue
+		}
+		i++
+		nums[i] = nums[j]
+	}
+	return i + 1
+}
