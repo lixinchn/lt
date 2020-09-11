5e7fd63a31611ec6e52608734002440103965ab3
lixin <lixin@windimg.com>
2020-09-11 15:18:34 +0800

    better 3sum solution

--- 015.3_sum/solution.go
+++ 015.3_sum/solution.go
@@ -1,31 +1,38 @@
 package leetcode
 
-import (
-	"sort"
-)
+import "sort"
 
 func threeSum(nums []int) [][]int {
 	m := [][]int{}
-	t := map[int]bool{}
 	sort.Ints(nums)
-	for i := 0; i < len(nums); i++ {
-		if _, ok := t[nums[i]]; !ok {
-			mm := map[int]int{}
-			prevAnother := 10000000
-			for j := i + 1; j < len(nums); j++ {
-				another := (nums[i] + nums[j]) * -1
-				if prevAnother != another {
-					if _, ok := mm[another]; ok {
-						prevAnother = another
-						m = append(m, []int{nums[j], nums[i], another})
-						t[nums[i]] = true
-					}
-				}
-				mm[nums[j]] = i
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		low, high, preLow, preHigh := i+1, len(nums)-1, -1, -1
+		for low < high {
+			if preLow > -1 && nums[preLow] == nums[low] {
+				low++
+				continue
+			}
+			if preHigh > -1 && nums[preHigh] == nums[high] {
+				high--
+				continue
+			}
+
+			temp := nums[i] + nums[low] + nums[high]
+			if temp == 0 {
+				preLow = low
+				preHigh = high
+				m = append(m, []int{nums[i], nums[low], nums[high]})
+				low++
+				high--
+			} else if temp < 0 {
+				low++
+			} else {
+				high--
 			}
 		}
 	}
 	return m
 }
-
-// -4, -1, -1, 0, 1, 2
