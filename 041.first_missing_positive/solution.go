9610f5625101abdd7319f91e5f0f72b7b2e117f0
lixin <lixin@windimg.com>
2020-09-16 15:34:32 +0800

    first missing positive

--- 041.first_missing_positive/solution.go
+++ 041.first_missing_positive/solution.go
@@ -1,4 +1,17 @@
 package leetcode
 
 func firstMissingPositive(nums []int) int {
+	for j := 0; j < len(nums); j++ {
+		for nums[j] < len(nums) && nums[j] > 0 && nums[nums[j]-1] != nums[j] {
+			temp := nums[nums[j]-1]
+			nums[nums[j]-1] = nums[j]
+			nums[j] = temp
+		}
+	}
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+	return len(nums) + 1
 }
---
+++ 041.first_missing_positive/solution_test.go
@@ -0,0 +1,57 @@
+package leetcode
+
+import "testing"
+
+func Test_firstMissingPositive(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "t1",
+			args: args{
+				nums: []int{1, 2, 0},
+			},
+			want: 3,
+		},
+		{
+			name: "t2",
+			args: args{
+				nums: []int{3, 4, -1, 1},
+			},
+			want: 2,
+		},
+		{
+			name: "t3",
+			args: args{
+				nums: []int{7, 8, 9, 10, 11},
+			},
+			want: 1,
+		},
+		{
+			name: "t4",
+			args: args{
+				nums: []int{-1, -2, 0},
+			},
+			want: 1,
+		},
+		{
+			name: "t5",
+			args: args{
+				nums: []int{2, 1},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstMissingPositive(tt.args.nums); got != tt.want {
+				t.Errorf("firstMissingPositive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
