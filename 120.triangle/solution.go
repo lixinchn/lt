3e62b96e921605b0c5e234112bacc4754ff02780
lixin <lixin@windimg.com>
2020-10-14 17:05:53 +0800

    triangle better

--- 120.triangle/solution.go
+++ 120.triangle/solution.go
@@ -4,28 +4,25 @@ func minimumTotal(triangle [][]int) int {
 	if len(triangle) == 0 {
 		return 0
 	}
-	res := [][]int{triangle[0]}
 	for i := 1; i < len(triangle); i++ {
-		line := make([]int, len(triangle[i]))
 		for j := 0; j < len(triangle[i]); j++ {
 			if j == 0 {
-				line[j] = res[i-1][0] + triangle[i][j]
+				triangle[i][j] += triangle[i-1][0]
 			} else if j == len(triangle[i])-1 {
-				line[j] = res[i-1][len(res[i-1])-1] + triangle[i][j]
+				triangle[i][j] += triangle[i-1][len(triangle[i-1])-1]
 			} else {
-				if res[i-1][j-1] >= res[i-1][j] {
-					line[j] = res[i-1][j] + triangle[i][j]
+				if triangle[i-1][j-1] >= triangle[i-1][j] {
+					triangle[i][j] += triangle[i-1][j]
 				} else {
-					line[j] = res[i-1][j-1] + triangle[i][j]
+					triangle[i][j] += triangle[i-1][j-1]
 				}
 			}
 		}
-		res = append(res, line)
 	}
-	ret := res[len(res)-1][0]
-	for i := 1; i < len(res[len(res)-1]); i++ {
-		if res[len(res)-1][i] < ret {
-			ret = res[len(res)-1][i]
+	ret := triangle[len(triangle)-1][0]
+	for i := 1; i < len(triangle[len(triangle)-1]); i++ {
+		if triangle[len(triangle)-1][i] < ret {
+			ret = triangle[len(triangle)-1][i]
 		}
 	}
 	return ret
